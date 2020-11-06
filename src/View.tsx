31c4af69bce56bd28e890feab5379b565300f5d2
L-Nikolai <unsense.wow@gmail.com>
2020-11-06 13:28:40 +0300

    Create table in View component

--- src/View.tsx
+++ src/View.tsx
@@ -3,22 +3,28 @@ import React from "react";
 const View = () => {
   const data = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 
-  const CellsList = (props:{data:number[]}): React.ReactElement => {
+  const CellsList = (props: { data: number[] }): React.ReactElement => {
     const data = props.data;
-    const renderCell = data.map((cell: number) => 
-      
+    const renderCell = data.map((cell: number) => (
       <td key={cell.toString()}>{cell}</td>
-    )
-        return <tr>{renderCell}</tr>;        
+    ));
+    return <tr>{renderCell}</tr>;
   };
 
-               
-  
+  const Board = () => {
+    return (
+      <>
+        {data.map(() => {
+          return <CellsList data={data} />;
+        })}
+      </>
+    );
+  };
 
   return (
-    <>
-      <CellsList data={data} />
-    </>
+    <table>
+      <Board />
+    </table>
   );
 };
 
